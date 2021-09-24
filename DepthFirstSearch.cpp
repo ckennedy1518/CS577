29781a5a8332bb50d6fa970d5def9ced20d26b13
ckennedy1518 <71947334+ckennedy1518@users.noreply.github.com>
2021-09-23 21:30:29 -0500

    Working DFS
    Major refactor #2: changed how unvisited nodes were tracked. Used different way to check if all nodes were visited.

--- DepthFirstSearch.cpp
+++ DepthFirstSearch.cpp
@@ -1,15 +1,25 @@
 #include <iostream> 
 #include <sstream>
-#include <vector> 
-#include <algorithm>
-#include <iterator>
+#include <vector>
 
 using namespace std; 
 
-vector<vector<string>> AdjacencyList; 
-vector<string> unvisitedNodes; 
-vector<string> visitedNodes;
-int numNodesRemoved = 0;
+vector<vector<string>> AdjacencyList;
+vector<bool> visitedNodes;
+
+// returns true if all nodes are visited, false otherwise
+bool AllNodesVisited()
+{
+    for (bool b : visitedNodes)
+    {
+        if (!b)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 // function returns the position in the 2D Adjacency List of the string it is passed or -1 if 
 // the string isn't the first element of one of the vector<string>s in the AL
@@ -28,24 +38,22 @@ int FindPosInList(string NodeToFind)
     return -1;
 }
 
-// Function recursively "pushes" and "pops" nodes off a "stack" to traverse the adjacency list
+// Function recursively visits nodes to traverse the adjacency list
 vector<string> DFS(string node) 
 {
+    int nodePosInList = FindPosInList(node);
     vector<string> output;    
     output.push_back(node);
-    visitedNodes.push_back(node);
-    unvisitedNodes.erase(find(unvisitedNodes.begin(), unvisitedNodes.end(), node));
-    numNodesRemoved++;
+    visitedNodes[nodePosInList] = true;
 
-    for (int i = 1; i < AdjacencyList[FindPosInList(node)].size(); i++) // for the children of node
+    for (int i = 1; i < AdjacencyList[nodePosInList].size(); i++) // for the children of node
     {
-        //check if child is in visitedNodes
-        if (*find(visitedNodes.begin(), visitedNodes.end(), AdjacencyList[FindPosInList(node)][i]) != AdjacencyList[FindPosInList(node)][i]) // child hasn't been visited
+        if (!visitedNodes[FindPosInList(AdjacencyList[nodePosInList][i])]) // child hasn't been visited
         {
-            vector<string> temp = DFS(AdjacencyList[FindPosInList(node)][i]);
-            for (string st : temp)
+            vector<string> temp = DFS(AdjacencyList[nodePosInList][i]);
+            for (string s : temp)
             {
-                output.push_back(st);
+                output.push_back(s);
             }
         }        
     } 
@@ -53,26 +61,24 @@ vector<string> DFS(string node)
     return output; 
 }
 
-// Function gets input for the graph, calls DFS to get the output information, then prints it to the screen
 int main() { 
     int numGraphs = 0; 
-    cin >> numGraphs; 
-    
-    vector<string> output; 
+    cin >> numGraphs;
+
+    vector<string> output;
     
     for (int i = 0; i < numGraphs; i++) 
     { 
         int numNodes = 0; 
         cin >> numNodes;
+ 
+        AdjacencyList.clear();
+        output.clear();
+        visitedNodes.clear();
 
-        unvisitedNodes.clear(); 
-        visitedNodes.clear(); 
-        AdjacencyList.clear(); 
-        output.clear(); 
-
-        for (int j = 0; j < numNodes; j++)  // loop gets nodes and edges 
+        for (int j = 0; j < numNodes; j++)  // loop gets nodes and edges (input)
         { 
-            string nodeAndAdjacentNodeList; 
+            string nodeAndAdjacentNodeList;
 
             if (j == 0)
             {
@@ -89,21 +95,37 @@ int main() {
             } 
             
             AdjacencyList.push_back(niceInput);
-            unvisitedNodes.push_back(AdjacencyList[AdjacencyList.size() - 1][0]);
+            visitedNodes.push_back(false);
         }
 
-        while (!unvisitedNodes.empty())
+        while (!AllNodesVisited()) // DFS portion
         {
-            // two step process here accounts for multiple possibilities
-            // if we were to just set output to be = to the next line it could get overwritten
-
-            DFS(AdjacencyList[FindPosInList(unvisitedNodes[0])][0]);
+            int nextUnvisited = 0;
+            for (bool b : visitedNodes)
+            {
+                if (!b)
+                {
+                    // two step process here accounts for disconnected graphs
+                    // if we were to just set output to be = to the next line it could get overwritten
+                    vector<string> temp;
+                    temp = DFS(AdjacencyList[nextUnvisited][0]);
+                    for (string s : temp)
+                    {
+                        output.push_back(s);
+                    }
+                    break;
+                }
+                else
+                {
+                    nextUnvisited++;
+                }
+            }
         }
         
-        for (int k = 0; k < visitedNodes.size(); k++)  // output 
+        for (int k = 0; k < output.size(); k++)  // output 
         { 
-            cout << visitedNodes[k]; 
-            (k == visitedNodes.size() - 1) ? cout << endl : cout << " "; // print new line or space depending on where at in vector
+            cout << output[k]; 
+            (k == output.size() - 1) ? cout << endl : cout << " "; // print new line or space depending on where at in vector
         } 
     } 
     
